83c213c7385a2fbe1ab064ecbbcdd2997e47b7af
Mert <mertimekci@gmail.com>
2023-11-25 21:10:14 +0100

    Store input data in object.

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ let sold = document.getElementById("sold");
 let soldPrice = document.getElementById("soldPrice");
 let winner = document.getElementById("winner");
 
+let acceptData = () => {
+    itemData["itemName"] = itemName.value;
+    itemData["expertisePrice"] = expertisePrice.value;
+    itemData["firstGuess"] = firstGuess.value;
+    itemData["secondGuess"] = secondGuess.value;
+    itemData["sold"] = sold.value;
+    itemData["soldPrice"] = soldPrice.value;
+    //itemData["winner"] = winner.value;
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); 
+    acceptData();
     console.log("save button clicked!")
-
+    console.log(itemData);
 })
