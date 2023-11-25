f817a5394d798c7b14712a1da96006af8392badd
Malwine <malwine@mailbox.org>
2023-11-25 20:49:46 +0100

    Get data from input

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let form = document.getElementById("inputContainer");
+let itemData = {};
+let itemName = document.getElementById("itemName");
+let expertisePrice = document.getElementById("expertisePrice");
+let firstGuess = document.getElementById("firstGuess");
+let secondGuess = document.getElementById("secondGuess");
+let sold = document.getElementById("sold");
+let soldPrice = document.getElementById("soldPrice");
+let winner = document.getElementById("winner");
 
 
 form.addEventListener("submit", (e) => {
