65c61c3be586e02953240b04e2e3f7512b3d1722
Malwine <malwine@mailbox.org>
2023-12-07 14:47:17 +0100

    Add min and max expertise value, clear form and validate item name

--- index.js
+++ index.js
@@ -2,38 +2,58 @@ let form = document.getElementById("inputContainer");
 let itemList = document.getElementById("items");
 let items = [];
 let itemName = document.getElementById("itemName");
-let expertisePrice = document.getElementById("expertisePrice");
+let exMin = document.getElementById("exMin");
+let exMax = document.getElementById("exMax");
 let firstGuess = document.getElementById("firstGuess");
 let secondGuess = document.getElementById("secondGuess");
 let sold = document.getElementById("sold");
 let soldPrice = document.getElementById("soldPrice");
+let message = document.getElementById("message");
 
 let acceptData = () => {
-    let itemData = {};
-    itemData["itemName"] = itemName.value;
-    itemData["expertisePrice"] = expertisePrice.value;
-    itemData["firstGuess"] = firstGuess.value;
-    itemData["secondGuess"] = secondGuess.value;
-    itemData["sold"] = sold.checked;
-    itemData["soldPrice"] = soldPrice.value;
-    items.push(itemData);
+    if (itemName.value !== "") {
+        let itemData = {};
+        itemData["itemName"] = itemName.value;
+        itemData["exMin"] = exMin.value;
+        itemData["exMax"] = exMax.value;
+        itemData["firstGuess"] = firstGuess.value;
+        itemData["secondGuess"] = secondGuess.value;
+        itemData["sold"] = sold.checked;
+        itemData["soldPrice"] = soldPrice.value;
+        items.push(itemData);
+    }
+}
+
+let clearForm = () => {
+    itemName.value = "";
+    exMin.value = "";
+    exMax.value = "";
+    firstGuess.value = "";
+    secondGuess.value = "";
+    sold.checked = false;
+    soldPrice.value = "";
 }
 
 let renderItems = () => {
-    let lastItem = items[items.length-1];
-    itemList.innerHTML +=
-        `<div class="item">
-            <div class="itemName">${lastItem.itemName}</div>
-            <div class="expertisePrice">${lastItem.expertisePrice}</div>
-            <div class="firstGuess">${lastItem.firstGuess}</div>
-            <div class="secondGuess">${lastItem.secondGuess}</div>
-            <div class="sold">${ lastItem.sold ? "<i class=\"fas fa-check-square\"></i>" : "<i class=\"fas fa-minus-square\"></i>" }</div>
-            <div class="soldPrice">${lastItem.sold ? lastItem.soldPrice : ""}</div>
-            <div class="options">
-            <div class="fas fa-edit"></div>
-            <div class="fas fa-trash-alt"></div>
-            </div>
-        </div>`;
+    if(items.length-1 > 0) {
+        let lastItem = items[items.length-1];
+        itemList.innerHTML +=
+            `<div class="item">
+                <div class="itemName">${lastItem.itemName}</div>
+                <div class="expertisePrice">${lastItem.exMin} - ${lastItem.exMax}</div>
+                <div class="firstGuess">${lastItem.firstGuess}</div>
+                <div class="secondGuess">${lastItem.secondGuess}</div>
+                <div class="sold">${ lastItem.sold ? "<i class=\"fas fa-check-square\"></i>" : "<i class=\"fas fa-minus-square\"></i>" }</div>
+                <div class="soldPrice">${lastItem.sold ? lastItem.soldPrice : "-"}</div>
+                <div class="options">
+                <div class="fas fa-edit"></div>
+                <div class="fas fa-trash-alt"></div>
+                </div>
+            </div>`;
+        clearForm();
+    } else {
+        message.innerHTML = "Eingabe ungültig";
+    }
 }
 
 form.addEventListener("submit", (e) => {
